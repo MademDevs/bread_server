3c6c62e2c385ba868843aee8f1ec11f527659f69
Eric <eric.muench@hof-university.de>
2021-05-14 18:24:54 +0200

    Test is working xD

--- src/main/kotlin/de/madem/repositories/DatabaseRepository.kt
+++ src/main/kotlin/de/madem/repositories/DatabaseRepository.kt
@@ -5,15 +5,15 @@ import de.madem.model.DBDishTable
 import org.ktorm.database.Database
 import org.ktorm.entity.map
 import org.ktorm.entity.sequenceOf
+import java.sql.DriverManager
 
 object DatabaseRepository {
     private val database : Database
 
     init {
-
-        database = Database.connect(
-            getDatabaseConnectionUrl()
-        )
+        database = Database.connect{
+            DriverManager.getConnection(getDatabaseConnectionUrl())
+        }
     }
 
     fun test() : List<DBDish>{
@@ -22,11 +22,19 @@ object DatabaseRepository {
 
     //#region private functions
     private fun getDatabaseConnectionUrl() : String{
-        val baseUrl = "baseUrl"
+
+        val baseUrl = "jdbc:mysql://localhost:3306/breaddb"
         val urlParams = mapOf<String,Any>(
+            "user" to "username",
+            "password" to "password",
             "driver" to "com.mysql.jdbc.Driver",
-            "serverTimezone" to "Europe/Berlin"
+            "useSSL" to false,
+            "serverTimezone" to "Europe/Berlin",
+            "allowPublicKeyRetrieval" to true
         )
+        //"verifyServerCertificate" to false,
+        //"useSSL" to true,
+        //"requireSSL" to true,
         //TODO: Add valid user + password and ensure that ssl works
 
         return "$baseUrl?${urlParams.toList().joinToString("&"){ (key,value) -> "$key=$value" }}".also { println(it) }
